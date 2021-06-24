387db1ae1f13b2f26d927132b9a3088627397aab
Diego Barbosa <diegobarbosamorim@gmail.com>
2021-06-24 18:10:27 -0300

    Update storeLinks.js

--- src/utils/storeLinks.js
+++ src/utils/storeLinks.js
@@ -16,13 +16,12 @@ export async function saveLink(key, newLink){
     const hasLinks = linksStored.some(link => link.id === newLink.id);
 
     if(hasLinks){
-        console.log('esse link ja existe na lista');
         return;//para execucao aqui
     }
 
     linksStored.push(newLink);
     await AsyncStorage.setItem(key, JSON.stringify(linksStored));
-    console.log('link salvo com sucesso')
+    
 }
 // deletar algum link especifico
 export async function deleteLink(links, id){
@@ -30,7 +29,7 @@ export async function deleteLink(links, id){
         return (item.id !== id) 
     })
     await AsyncStorage.setItem('sujeitolinks', JSON.stringify(myLinks))
-    console.log('link deletado do storage')
+    
 
     return myLinks;
 
